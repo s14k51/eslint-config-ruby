40e5eb56de2d2406822b094bd2a56b2ac96d2ca5
s14k51 <s14k51@gmail.com>
2020-03-27 20:20:45 -0500

    refactor(logicalpadding): remove plugin

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 module.exports = {
   extends: [
     'airbnb-base',
-    'plugin:logical-padding/error',
     'plugin:unicorn/recommended',
     'plugin:you-dont-need-lodash-underscore/compatible',
     'plugin:sonarjs/recommended',
   ],
   rules: {
+    'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 1, maxEOF: 0 }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'cjs-export', next: '*' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      { blankLine: 'always', prev: 'cjs-import', next: '*' },
+      { blankLine: 'never', prev: 'cjs-import', next: 'cjs-import' },
+      { blankLine: 'always', prev: 'function', next: '*' },
+      { blankLine: 'always', prev: 'expression', next: '*' },
+      { blankLine: 'any', prev: 'expression', next: 'expression' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
     'unicorn/custom-error-definition': 'error',
     'unicorn/no-unsafe-regex': 'error',
     'unicorn/no-unused-properties': 'error',
