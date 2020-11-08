ba825636fbf6afdb4df85af3da606312e179baba
s14k51 <s14k51@gmail.com>
2020-11-08 15:43:01 -0600

    feat!: use single config file
    BREAKING CHANGE: projects can now use a single .eslint file for both
    source and test files

---
+++ base.js
@@ -0,0 +1,47 @@
+module.exports = {
+  ignorePatterns: [
+    'node_modules/',
+    'coverage/',
+  ],
+  extends: [
+    'airbnb-base',
+    'plugin:unicorn/recommended',
+    'plugin:you-dont-need-lodash-underscore/compatible',
+    'plugin:sonarjs/recommended',
+    'plugin:strict-case/recommended',
+  ],
+  rules: {
+    'no-multiple-empty-lines': [
+      'error',
+      { max: 1, maxBOF: 1, maxEOF: 0 },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'cjs-export', next: '*' },
+      { blankLine: 'any', prev: 'cjs-export', next: 'cjs-export' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      { blankLine: 'always', prev: 'cjs-import', next: '*' },
+      { blankLine: 'any', prev: 'cjs-import', next: 'cjs-import' },
+      { blankLine: 'any', prev: 'expression', next: '*' },
+      { blankLine: 'any', prev: 'expression', next: 'expression' },
+      { blankLine: 'any', prev: 'block-like', next: '*' },
+      { blankLine: 'any', prev: 'block-like', next: 'block-like' },
+      { blankLine: 'always', prev: '*', next: 'function' },
+      { blankLine: 'always', prev: 'function', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'class' },
+      { blankLine: 'always', prev: 'class', next: '*' },
+      { blankLine: 'any', prev: '*', next: 'return' },
+    ],
+    'unicorn/custom-error-definition': 'error',
+    'unicorn/no-unsafe-regex': 'error',
+    'unicorn/no-unused-properties': 'error',
+    'unicorn/no-fn-reference-in-iterator': 'error',
+    'unicorn/no-unreadable-array-destructuring': 'off',
+    'unicorn/filename-case': [
+      'error',
+      { cases: { kebabCase: true, camelCase: true, pascalCase: true } },
+    ],
+    'sonarjs/no-duplicate-string': 'off',
+  },
+};
